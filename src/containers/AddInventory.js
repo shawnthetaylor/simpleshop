b667cbbf96bc6327952641b47df8526ccee9983b
Shawn Taylor <shawn.b.taylor23@gmail.com>
2017-04-01 22:47:00 -0700

    Created container for AddInventory form

---
+++ src/containers/AddInventory.js
@@ -0,0 +1,69 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import addInventory from '../actions/addInventory';
+
+const propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+class AddInventory extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    const item = {
+      name: this.name.value,
+      price: this.price.value,
+      quantity: this.quantity.value
+    };
+    this.props.dispatch(addInventory(item));
+
+    // Reset form values
+    this.name.value = '';
+    this.price.value = '';
+    this.quantity.value = '';
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.onFormSubmit }>
+        <label>
+          What is the name of the product?
+          <input type='text'
+                 name='name'
+                 ref={ node => { this.name = node; }}
+                 required />
+        </label>
+        <label>
+          What is the price per unit of this product?
+          <input type='number'
+                 name='price'
+                 min='0.01'
+                 step='0.01'
+                 name='price'
+                 ref={ node => { this.price = node; }}
+                 required />
+        </label>
+        <label>
+          How many units would you like to add?
+          <input type='number'
+                 min='0'
+                 name='quantity'
+                 ref={ node => { this.quantity = node; }}
+                 required />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    );
+  }
+}
+
+AddInventory.propTypes = propTypes
+AddInventory = connect()(AddInventory);
+
+export default AddInventory;
