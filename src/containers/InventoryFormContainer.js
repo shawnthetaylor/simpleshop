ab8b4e3953c1e7c7c4aad2e2943abd5480bdb3ad
Shawn Taylor <shawn.b.taylor23@gmail.com>
2017-04-03 15:41:34 -0700

    Pass in more props to InventoryForm for duplicate detection

--- src/containers/InventoryFormContainer.js
+++ src/containers/InventoryFormContainer.js
@@ -1,17 +1,25 @@
 import { connect } from 'react-redux';
 import InventoryForm from '../components/InventoryForm';
 import createInventory from '../actions/createInventory';
+import addInventory from '../actions/addInventory';
+
+const mapStateToProps = state => {
+  return { inventory: state.inventory };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFormSubmit: item => {
+    onCreateItem: item => {
       dispatch(createInventory(item));
+    },
+    onUpdateItem: (name, dq) => {
+      dispatch(addInventory(name, dq));
     }
   };
 };
 
 const InventoryFormContainer = connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(InventoryForm);
 
